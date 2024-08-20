26692364a5199fad8d1332f706fa43bef81a76de
TusharSSurve <ttusharsurve567@gmail.com>
2024-08-20 21:42:30 +0530

    [master] : Events Page added

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,10 @@ const nextConfig = {
       {
         hostname: 'bytegrad.com',
         protocol: 'https'
+      },
+      {
+        hostname: 'images.unsplash.com',
+        protocol: 'https'
       }
     ]
   }
--- src/components/h1.tsx
+++ src/components/h1.tsx
@@ -1,7 +1,10 @@
 import { ChildrenProps } from "@/lib/types";
-
-export default function H1({ children }: ChildrenProps) {
+import { twMerge } from "tailwind-merge";
+type H1Props = {
+  className: string;
+} & ChildrenProps
+export default function H1({ className, children }: H1Props) {
   return (
-    <h1 className="text-3xl lg:text-6xl font-bold tracking-tight">{children}</h1>
+    <h1 className={twMerge("text-3xl lg:text-6xl font-bold tracking-tight", className)}>{children}</h1>
   )
 }
